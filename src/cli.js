091b8591e2883de4135a2a3e868174a9959b2b35
Darell Hoei <darellhoei@gmail.com>
2021-06-19 18:09:15 +0700

    prompts and autofill

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,68 @@
-export function cli(args) {
-  console.log(args);
+import arg from "arg";
+import inquirer from "inquirer";
+
+function parseArgumentsIntoOptions(rawArgs) {
+  const args = arg(
+    {
+      "--git": Boolean,
+      "--yes": Boolean,
+      "--install": Boolean,
+      "-g": "--git",
+      "-y": "--yes",
+      "-i": "--install",
+    },
+    {
+      argv: rawArgs.slice(2),
+    }
+  );
+
+  return {
+    skipPrompts: args["--yes"] || false,
+    git: args["--git"] || false,
+    template: args._[0],
+    runInstall: args["--install"] || false,
+  };
+}
+
+async function promptForMissingOptions(opts) {
+  const defaultTemplate = "JavaScript";
+  if (opts.skipPrompts) {
+    return {
+      ...opts,
+      template: opts.template || defaultTemplate,
+    };
+  }
+
+  const questions = [];
+  if (!opts.template) {
+    questions.push({
+      type: "list",
+      name: "template",
+      message: "Please choose which project template to use",
+      choices: ["JavaScript", "TypeScript"],
+      default: defaultTemplate,
+    });
+  }
+
+  if (!opts.git) {
+    questions.push({
+      type: "confirm",
+      name: "git",
+      message: "Initialize a git repository?",
+      default: false,
+    });
+  }
+
+  const answers = await inquirer.prompt(questions);
+  return {
+    ...opts,
+    template: opts.template || answers.template,
+    git: opts.git || answers.git,
+  };
+}
+
+export async function cli(args) {
+  let options = parseArgumentsIntoOptions(args);
+  options = await promptForMissingOptions(options);
+  console.log(options);
 }
